4b24cd1e20a00750b42e93ae9dd8872c6906d1ef
flavio <flaviomargarito@gmail.com>
2017-11-08 01:20:10 -0200

    Changing file.

--- R/runExams.R
+++ R/runExams.R
@@ -1,9 +1,24 @@
+#' @import shiny
+#' @import shinyAce
+#' @import shinyTree
+#' @import shinyFiles
+#' @import tools
+#' @import exams
 #' @export
-runExams <- function() {
+createExams <- function() {
+
+  ## Input handler for questions
+  registerInputHandler("shinyjsexamples.chooser", function(data, ...) {
+    if(is.null(data))
+      NULL
+    else
+      list(questions=as.character(data$questions), left=as.character(data$left), right=as.character(data$right))
+  }, force = TRUE)
+
   appDir <- system.file("shiny-exams", "examsapp", package = "shinyExams")
   if (appDir == "") {
     stop("Could not find app directory. Try re-installing `shinyExams`.", call. = FALSE)
   }
 
-  shiny::runApp(appDir, display.mode = "normal")
+  shiny::runApp(appDir = appDir, display.mode = "normal")
 }
